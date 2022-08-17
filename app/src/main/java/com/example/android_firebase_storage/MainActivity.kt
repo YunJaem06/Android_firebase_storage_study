8b011e1695e4fa53c593ae5fc0fe5f0819459ac2
YunJaem06 <yunjae0616@gmail.com>
2022-08-17 21:05:26 +0900

    firebase storage 오류까지 해결완료

--- app/src/main/java/com/example/android_firebase_storage/MainActivity.kt
+++ app/src/main/java/com/example/android_firebase_storage/MainActivity.kt
@@ -1,11 +1,73 @@
 package com.example.android_firebase_storage
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.android_firebase_storage.databinding.ActivityMainBinding
+import com.google.firebase.storage.FirebaseStorage
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var ImageUri : Uri
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainFirebaseImg.setOnClickListener {
+
+            selectImage()
+        }
+        binding.btnMainImgUploading.setOnClickListener {
+
+            uploadImage()
+        }
+    }
+
+    private fun uploadImage() {
+
+        val formatter = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.getDefault())
+        val now = Date()
+        val fileName = formatter.format(now)
+
+        val storageReference = FirebaseStorage.getInstance().reference.child("image/$fileName")
+
+        storageReference.putFile(ImageUri)
+            .addOnSuccessListener {
+
+                binding.mainFirebaseImg.setImageURI(null)
+                Toast.makeText(this@MainActivity, "업로드 성공", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this@MainActivity, "업로드 실패", Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
+    private fun selectImage() {
+
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+
+        startActivityForResult(intent, 100)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 100 && resultCode == RESULT_OK){
+
+            ImageUri = data?.data!!
+            binding.mainFirebaseImg.setImageURI(ImageUri)
+        }
     }
 }
